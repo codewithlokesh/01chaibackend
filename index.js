fd8d30fb3b3cb7f20cc55f5fd3b79bcce1df0a69
codewithlokesh <lokeshingle2020@mail.com>
2025-08-06 17:35:36 +0530

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.get("/",(req, res)=>{
     res.send("<h2>hello express users</h2>")
 })
@@ -15,5 +15,5 @@ app.get("/admin",(req, res)=>{
     res.send("<h2>Admin here</h2>")
 })
 app.listen(port, ()=>{
-    console.log(`server start on port ${process.env.PORT}`)
+    console.log(`server startED on port ${process.env.PORT}`)
 })
